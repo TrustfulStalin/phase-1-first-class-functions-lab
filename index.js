f9d8650a728f9a865cc88343f65ea98073802eaf
Tevon Talbert <TevonTalbert@hotmail.com>
2023-10-03 19:19:56 -0500

    adding file

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(roman)
+{
+ 
+  let drivers = [...roman.slice(0,2)];
+
+  return drivers;
+}
+
+function returnLastTwoDrivers(roman)
+{
+
+  let drivers = [...roman.slice(2,4)];
+
+  return drivers;
+
+
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int)
+{
+  return function (fare = 5) {
+    return 5 * int;
+  }
+}
+
+const fareDoubler = function (fare) {
+  return fare * 2;
+}
+
+const fareTripler = function (fare) {
+  return fare * 3;
+}
+
+function selectDifferentDrivers(driversArray, fn) {
+  return fn(driversArray);
+}
